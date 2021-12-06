23bab5f9b5345e263b9ccd57214bcf948cac8367
xencodes <xencodes@icloud.com>
2021-12-06 15:29:09 +0530

    feat: init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Ion is running</title>
+			</Head>
+		</>
+	);
+};
+
+export default Home;
